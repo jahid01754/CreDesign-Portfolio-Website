30de3161864f65a34c6da34ce4c4399f5a91609c
Jahid <jahidulhasan01754@gmail.com>
2025-05-11 12:29:44 +0600

    myblog section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import AboutSection from "@/Components/AboutSection/AboutSection";
 import ClientFeedback from "@/Components/ClientFeedBack/ClientFeedBack";
 import HomeBanner from "@/Components/HomeBanner/HomeBanner";
+import MyBLog from "@/Components/MyBlog/MyBlog";
 import MyPortfolio from "@/Components/MyPortfolio/MyPortfolio";
 import NavBar from "@/Components/NavBar/NavBar";
 import Partners from "@/Components/Partners/Partners";
@@ -20,6 +21,7 @@ export default function Home() {
         <ClientFeedback />
         <MyPortfolio />
         <Partners />
+        <MyBLog />
       </div>
     </>
   );
